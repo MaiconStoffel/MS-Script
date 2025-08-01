a29a578371967c462063a802cad1cadead1c0ad6
MaiconStoffel <128558232+MaiconStoffel@users.noreply.github.com>
2025-08-01 16:40:48 -0300

    1/08

---
+++ Test-i5pl4m/main.js
@@ -0,0 +1,10 @@
+document.getElementById("btnAspas").addEventListener("click", () => {
+  alert("Executou FUNÇÃO A");
+  // aqui chama sua função que manipula o InDesign via UXP
+});
+
+document.getElementById("btnCapitalizacao").addEventListener("click", () => {
+  alert("Executou FUNÇÃO B");
+  // chama função de capitalização
+});
+
